0e9b1dfe91e42c96bc398e09f1e198edbe49aab2
Clovis Fritzen <clovisf@gmail.com>
2025-03-14 08:27:07 -0300

    edited code

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # interrupt tutorial: https://electrocredible.com/raspberry-pi-pico-external-interrupts-button-micropython/
-from machine import Pin
+from machine import Pin, Timer
 import time
 
-#button = Pin(14,Pin.IN,Pin.PULL_UP) #Pi Pico 2
-button = Pin(26,Pin.IN,Pin.PULL_UP) #Xiao RP2350
-#led= machine.Pin(15, machine.Pin.OUT) #Pi Pico 2
-led= machine.Pin(27, machine.Pin.OUT) #Xiao RP2350
+button = Pin(14,Pin.IN,Pin.PULL_UP) #Pi Pico 2
+#button = Pin(26,Pin.IN,Pin.PULL_UP) #Xiao RP2350
+led= machine.Pin(15, machine.Pin.OUT) #Pi Pico 2
+#led= machine.Pin(27, machine.Pin.OUT) #Xiao RP2350
 flowcontroltime= time.ticks_ms()
 buttontime= time.ticks_ms()
 
@@ -24,18 +24,22 @@ def myFunction(button):
             shouldblink = 1
         else:
             shouldblink = 0
-
-while True:
-    button.irq(trigger=Pin.IRQ_RISING, handler=myFunction)
-    
-    # non-blocking code from: https://fritzenlab.net/2025/03/11/ds18b20-temperature-sensor-with-micropython/
-    if time.ticks_diff(time.ticks_ms(), flowcontroltime) > 200: # this IF will be true every 200 ms
-            flowcontroltime= time.ticks_ms() #update with the "current" time
             
-            if shouldblink == 0:
-                led.value(0)
-            else:
-                if led.value() == 0:
-                    led.value(1)
-                else:
-                    led.value(0)
+def led_interrupt(timer):
+    if shouldblink == 1:
+        led.value(not led.value())
+
+if __name__ == "__main__":
+    # timer interrupt ideas from here: https://randomnerdtutorials.com/micropython-timer-interrupts-ep32-esp8266/
+    tim= Timer(-1)
+    tim.init(mode=Timer.PERIODIC, period=200, callback=led_interrupt)
+    
+    while True:
+        button.irq(trigger=Pin.IRQ_RISING, handler=myFunction)
+        
+        # non-blocking code from: https://fritzenlab.net/2025/03/11/ds18b20-temperature-sensor-with-micropython/
+        if time.ticks_diff(time.ticks_ms(), flowcontroltime) > 200: # this IF will be true every 200 ms
+                flowcontroltime= time.ticks_ms() #update with the "current" time
+                
+                
+                    
